154f72dd49d302e47b304c8d983cebd2b1437096
IZA17203 <IZA17203@open-groupe.com>
2024-12-02 15:34:00 +0100

    add terraform

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,14 +8,6 @@
 
 resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
-
-   tags = "${
-    tomap({
-    Name                             = "terraform-eks-demo-node"
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
-   })
-   }
-   
 }
 
 resource "aws_subnet" "demo" {
@@ -24,22 +16,10 @@ resource "aws_subnet" "demo" {
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   cidr_block        = "10.0.${count.index}.0/24"
   vpc_id            = "${aws_vpc.demo.id}"
-
-  tags = "${
-    tomap({
-      Name                             = "terraform-eks-demo-node"
-      "kubernetes.io/cluster/${var.cluster-name}" = "shared"
-})
-  }"
 }
 
 resource "aws_internet_gateway" "demo" {
   vpc_id = "${aws_vpc.demo.id}"
-  
-  tags = "${
-    tomap({
-       Name = "terraform-eks-demo"
-   })
 }
 
 resource "aws_route_table" "demo" {
